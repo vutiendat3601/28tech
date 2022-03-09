d71cbd104bafa5c422cba5e3c84a325cd81cfdf4
vutiendat3601 <vutien.dat.3601@gmail.com>
2022-03-09 20:37:27 +0700

    set map

--- dsa/array/vectordemo.cpp
+++ dsa/array/vectordemo.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 
-int main() {
-    vector<int> a;
-    for (int i=0;i<10;i++)
+int main()
+{
+    vector<int> a(10);
+    for (int i = 0; i < 10; i++)
         a.push_back(i);
-    for (vector<int>::iterator it = a.begin(); it < a.end();it++) {
+    for (vector<int>::iterator it = a.begin(); it < a.end(); it++)
         cout << *it << " ";
-    }
 }
